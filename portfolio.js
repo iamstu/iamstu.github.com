5391be7b9c7f35b6e184a286db38de0dfad94482
unknown <stuartalafollette@gmail.com>
2018-07-13 22:17:14 -0500

    adding in friend finder and burger apps

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,27 @@
 var linkImg = null;
+var viewState = "about";
 
-$(".pjct-img").on("click", function(){
+$("body").on("click", ".pjct-img", function(){
     linkImg = $("<figure><a href='" + $(this).attr("data-img") + "'>" + "<img src='" + $(this).attr("src") + "'>" + "</a></figure>");
     $(".link-area").html(linkImg);
     $(linkImg).addClass("link-img");
     $(linkImg).find("img").addClass("link-img");
 });
 
+$(".toggle-btns").on("click", ".display-btn", function(){
+    console.log(this.textContent);
+    viewState = this.textContent.toLowerCase();
+    console.log(viewState);
+    toggleView();
+});
+ 
+function toggleView(){
+    if (viewState === "about"){
+        $(".pjcts").hide();
+        $(".about").show();
+    }else{
+        $(".about").hide();
+        $(".pjcts").show();
+    }
+}
+toggleView();
